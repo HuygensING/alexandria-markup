89c105948d3d9d3e0d73b7adfbd8c4586a77b2b5
Bram Buitendijk <bram.buitendijk@huygens.knaw.nl>
2017-01-27 13:13:14 +0100

    [NLA-347] Implement importLMNL just enough to satisfy testTextRangeAnnotation

--- src/main/java/lmnl_importer/LMNLImporter.java
+++ src/main/java/lmnl_importer/LMNLImporter.java
@@ -1,18 +1,59 @@
 package lmnl_importer;
 
 import data_model.Document;
+import data_model.Limen;
+import data_model.TextNode;
+import data_model.TextRange;
 import lmnl_antlr.LMNLLexer;
 import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.Token;
+
+import java.util.Stack;
 
 /**
  * Created by Ronald Haentjens Dekker on 29/12/16.
  */
 public class LMNLImporter {
 
-    public Document importLMNL(String input) {
-        LMNLLexer lexer = new LMNLLexer(new ANTLRInputStream(input));
-        //TODO: build the data model using classes from the data_model package
-        return null;
-    }
+  public Document importLMNL(String input) {
+    LMNLLexer lexer = new LMNLLexer(new ANTLRInputStream(input));
+    Document document = new Document();
+    Limen limen = document.value();
+    Token token;
+    Stack<TextRange> textRangeCache = new Stack<>();
+    do {
+      token = lexer.nextToken();
+      if (token.getType() != Token.EOF) {
+        String ruleName = lexer.getRuleNames()[token.getType() - 1];
+        String modeName = lexer.getModeNames()[lexer._mode];
+        System.out.println(token + ": " + ruleName + ": " + modeName);
+        switch (ruleName) {
+          case "BEGIN_OPEN_RANGE":
+            break;
+          case "Name_Open_Range":
+            TextRange textRange = new TextRange(limen, token.getText());
+            textRangeCache.push(textRange);
+            break;
+          case "END_OPEN_RANGE":
+            break;
+          case "TEXT":
+            TextNode textNode = new TextNode(token.getText());
+            textRangeCache.peek().addTextNode(textNode);
+            limen.addTextNode(textNode);
+            break;
+          case "BEGIN_CLOSE_RANGE":
+            break;
+          case "Name_Close_Range":
+            break;
+          case "END_CLOSE_RANGE":
+            textRangeCache.pop();
+            break;
+          default:
+            break;
+        }
+      }
+    } while (token.getType() != Token.EOF);
+    return document;
+  }
 
 }
