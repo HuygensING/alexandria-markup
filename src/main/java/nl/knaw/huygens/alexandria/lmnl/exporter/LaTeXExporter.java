db0a6e77e70212a98fec8445fe71d9194baf392e
Bram Buitendijk <bram.buitendijk@huygens.knaw.nl>
2017-02-15 17:39:15 +0100

    [NLA-353] Generate latex graph from imported document

---
+++ src/main/java/nl/knaw/huygens/alexandria/lmnl/exporter/LaTeXExporter.java
@@ -0,0 +1,108 @@
+package nl.knaw.huygens.alexandria.lmnl.exporter;
+
+import nl.knaw.huygens.alexandria.lmnl.data_model.Annotation;
+import nl.knaw.huygens.alexandria.lmnl.data_model.Document;
+import nl.knaw.huygens.alexandria.lmnl.data_model.Limen;
+import nl.knaw.huygens.alexandria.lmnl.data_model.TextRange;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * Created by bramb on 07/02/2017.
+ */
+public class LaTeXExporter {
+  private static Logger LOG = LoggerFactory.getLogger(LaTeXExporter.class);
+
+  public String toLaTeX(Document document) {
+    StringBuilder latexBuilder = new StringBuilder();
+    latexBuilder.append("\\documentclass{article}\n")
+            .append("\\usepackage{tikz}\n")
+            .append("\\usepackage{latexsym}\n")
+            .append("\\usepackage[utf8x]{inputenc}\n")
+            .append("\\usetikzlibrary{arrows,decorations.pathmorphing,backgrounds,positioning,fit,petri}\n")
+            .append("\n").append("\\begin{document}\n")
+            .append("  \\pagenumbering{gobble}% Remove page numbers (and reset to 1)\n")
+            .append("  \\begin{tikzpicture}\n")
+            .append("    [textnode/.style={rectangle,draw=black!50,thick},\n")
+            .append("     textrange/.style={rectangle,draw=blue!50,thick},\n")
+            .append("     document/.style={circle,draw=black!50,thick}]\n")
+            .append("    \\node[document] (doc) {document};\n");
+    Limen limen = document.value();
+    appendLimen(latexBuilder, limen);
+    latexBuilder.append("  \\end{tikzpicture}\n")
+            .append("\\end{document}\n");
+    return latexBuilder.toString();
+  }
+
+  private void appendLimen(StringBuilder latexBuilder, Limen limen) {
+    if (limen != null) {
+      Set<TextRange> openTextRanges = new LinkedHashSet<>();
+      AtomicInteger textNodeCounter = new AtomicInteger(0);
+      AtomicInteger textRangeCounter = new AtomicInteger(0);
+      Map<TextRange,Integer> textRangeIndices = new HashMap<>();
+      limen.getTextNodeIterator().forEachRemaining(tn -> {
+        int i = textNodeCounter.getAndIncrement();
+        List<TextRange> textRanges = limen.getTextRanges(tn);
+
+        List<TextRange> toClose = new ArrayList<>();
+        toClose.addAll(openTextRanges);
+        toClose.removeAll(textRanges);
+        Collections.reverse(toClose);
+
+        List<TextRange> toOpen = new ArrayList<>();
+        toOpen.addAll(textRanges);
+        toOpen.removeAll(openTextRanges);
+
+        openTextRanges.removeAll(toClose);
+        openTextRanges.addAll(toOpen);
+
+        String content = tn.getContent().replaceAll(" ", "\\\\textvisiblespace ").replaceAll("\n", "[NL]");
+        String relPos = i == 0 ? "below=of doc" : ("right=of tn" + (i - 1));
+        String nodeLine = "    \\node[textnode] (tn" + i + ") [" + relPos + "] {" + content + "};\n";
+        latexBuilder.append(nodeLine);
+//        String drawDocLine = "    \\draw [->] (doc) -- (tn" + i + ");\n";
+//        latexBuilder.append(drawDocLine);
+        if (textNodeCounter.get() > 1) {
+          String drawLine = "    \\draw [->] (tn" + (i - 1) + ") -- (tn" + i + ");\n";
+          latexBuilder.append(drawLine);
+        }
+        toClose.forEach(tr -> {
+          int textRangeIndex = textRangeIndices.get(tr);
+          String drawLine = "    \\draw [->] (tr" + textRangeIndex + ") -- (tn" + (i-1) + ");\n";
+          latexBuilder.append(drawLine);
+        });
+        toOpen.forEach(tr -> {
+          textRangeIndices.put(tr,textRangeCounter.get());
+          int textRangeIndex = textRangeCounter.getAndIncrement();
+          String relPos2 = textRangeIndex == 0 ? "below=of tn" + i : ("right=of tr" + (textRangeIndex - 1));
+          String textRangeLine = "    \\node[textrange] (tr" + textRangeIndex + ") [" + relPos2 + "] {" + tr.getTag() + "};\n";
+          latexBuilder.append(textRangeLine);
+          String drawLine = "    \\draw [->] (tr" + textRangeIndex + ") -- (tn" + i + ");\n";
+          latexBuilder.append(drawLine);
+        });
+      });
+//      openTextRanges.forEach(tr -> latexBuilder.append(toCloseTag(tr)));
+    }
+  }
+
+
+  public StringBuilder toLMNL(Annotation annotation) {
+    StringBuilder annotationBuilder = new StringBuilder("[").append(annotation.getTag());
+    annotation.getAnnotations().forEach(a1 ->
+            annotationBuilder.append(" ").append(toLMNL(a1))
+    );
+    Limen limen = annotation.value();
+    if (limen.hasTextNodes()) {
+      annotationBuilder.append("}");
+      appendLimen(annotationBuilder, limen);
+      annotationBuilder.append("{").append(annotation.getTag()).append("]");
+    } else {
+      annotationBuilder.append("]");
+    }
+    return annotationBuilder;
+  }
+
+}
---
+++ src/test/java/nl/knaw/huygens/alexandria/lmnl/exporter/LaTexExporterTest.java
@@ -0,0 +1,60 @@
+package nl.knaw.huygens.alexandria.lmnl.exporter;
+
+import nl.knaw.huygens.alexandria.lmnl.data_model.Document;
+import nl.knaw.huygens.alexandria.lmnl.importer.LMNLImporter;
+import org.apache.commons.io.FileUtils;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Created by bramb on 15-2-2017.
+ */
+public class LaTexExporterTest {
+  private static Logger LOG = LoggerFactory.getLogger(LaTeXExporter.class);
+
+  @Test
+  public void testLaTeXOutput1() {
+    String laTeX = laTeXFromLMNLString("[l [n}144{n]}He manages to keep the upper hand{l]");
+    LOG.info("latex=\n{}", laTeX);
+    assertThat(laTeX).isNotBlank();
+  }
+
+  @Test
+  public void testLaTeXOutput2() throws IOException {
+    String laTeX = laTeXFromLMNLFile("data/1kings12.lmnl");
+    LOG.info("latex=\n{}", laTeX);
+    assertThat(laTeX).isNotBlank();
+  }
+
+  @Test
+  public void testLaTeXOutput3() throws IOException {
+    String laTeX = laTeXFromLMNLFile("data/ozymandias-voices-wap.lmnl");
+    LOG.info("latex=\n{}", laTeX);
+    assertThat(laTeX).isNotBlank();
+  }
+
+  private String laTeXFromLMNLString(String input) {
+    Document document = new LMNLImporter().importLMNL(input);
+    return toLaTeX(document);
+  }
+
+  private String laTeXFromLMNLFile(String pathname) throws IOException {
+    InputStream input = FileUtils.openInputStream(new File(pathname));
+    Document document = new LMNLImporter().importLMNL(input);
+    return toLaTeX(document);
+  }
+
+  private String toLaTeX(Document document) {
+    LaTeXExporter exporter = new LaTeXExporter();
+    return exporter.toLaTeX(document);
+  }
+}
+
+//  String pathname = ;
