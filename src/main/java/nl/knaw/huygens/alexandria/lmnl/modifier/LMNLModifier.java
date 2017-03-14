dad3d599f97eff0fc3d51878e4ef2ea9f0238846
Bram Buitendijk <bram.buitendijk@huygens.knaw.nl>
2017-03-14 15:45:37 +0100

    [NLA-365] Further complicate junit test

--- src/main/java/nl/knaw/huygens/alexandria/lmnl/modifier/LMNLModifier.java
+++ src/main/java/nl/knaw/huygens/alexandria/lmnl/modifier/LMNLModifier.java
@@ -7,6 +7,9 @@ import nl.knaw.huygens.alexandria.lmnl.data_model.Limen;
 import nl.knaw.huygens.alexandria.lmnl.data_model.TextNode;
 import nl.knaw.huygens.alexandria.lmnl.data_model.TextRange;
 
+import java.util.Collection;
+import java.util.List;
+
 public class LMNLModifier {
 
   Logger LOG = LoggerFactory.getLogger(LMNLModifier.class);
@@ -18,6 +21,9 @@ public class LMNLModifier {
   }
 
   public void addTextRange(TextRange newTextRange, Position position) {
+    logTextNodes(limen.textNodeList);
+    logTextRanges(limen.textRangeList);
+
     TextNodeCursor cursor = new TextNodeCursor(limen);
     int startOffset = position.getOffset();
     int endOffset = position.getOffset() + position.getLength();
@@ -27,6 +33,7 @@ public class LMNLModifier {
     int currentTextNodeLength = 0;
     // get to starting TextNode
     int offsetAtEndOfCurrentTextNode = currentOffset + currentTextNodeLength;
+//    logTextNodes(limen.textNodeList);
     while (goOn) {
       currentText = cursor.getCurrentText();
       currentTextNodeLength = cursor.getCurrentTextLength();
@@ -34,19 +41,31 @@ public class LMNLModifier {
       if (startOffset <= offsetAtEndOfCurrentTextNode) {
         int tailLength = Math.min(offsetAtEndOfCurrentTextNode, offsetAtEndOfCurrentTextNode - startOffset);
         int headLength = currentTextNodeLength - tailLength;
-        String headText = currentText.substring(0, headLength);
-        String tailText = currentText.substring(headLength);
         if (headLength > 0) {
-          // detach head
-          cursor.getCurrentTextNode().setContent(headText);
-          TextNode newTextNode = new TextNode(tailText);
-          TextNode nextTextNode = cursor.getCurrentTextNode().getNextTextNode();
-          newTextNode.setPreviousTextNode(cursor.getCurrentTextNode());
-          newTextNode.setNextTextNode(nextTextNode);
-          cursor.getCurrentTextNode().setNextTextNode(newTextNode);
-          limen.getTextRanges(cursor.getCurrentTextNode()).forEach(tr -> limen.associateTextWithRange(newTextNode, tr));
-          limen.associateTextWithRange(newTextNode, newTextRange);
-          limen.textNodeList.add(cursor.getTextNodeIndex() + 1, newTextNode);
+          if (tailLength > 0) {
+            // detach head
+            String headText = currentText.substring(0, headLength);
+            String tailText = currentText.substring(headLength);
+            cursor.getCurrentTextNode().setContent(headText);
+            TextNode newTailNode = new TextNode(tailText);
+            TextNode nextTextNode = cursor.getCurrentTextNode().getNextTextNode();
+            newTailNode.setPreviousTextNode(cursor.getCurrentTextNode());
+            newTailNode.setNextTextNode(nextTextNode);
+            if (nextTextNode != null) {
+              nextTextNode.setPreviousTextNode(newTailNode);
+            }
+            cursor.getCurrentTextNode().setNextTextNode(newTailNode);
+            limen.getTextRanges(cursor.getCurrentTextNode()).forEach(tr -> {
+              tr.addTextNode(newTailNode);
+              limen.associateTextWithRange(newTailNode, tr);
+            });
+            newTextRange.addTextNode(newTailNode);
+            limen.associateTextWithRange(newTailNode, newTextRange);
+            limen.textNodeList.add(cursor.getTextNodeIndex() + 1, newTailNode);
+          } else {
+            newTextRange.addTextNode(cursor.getCurrentTextNode());
+            limen.associateTextWithRange(cursor.getCurrentTextNode(), newTextRange);
+          }
           cursor.advance();
         }
         goOn = false;
@@ -57,9 +76,10 @@ public class LMNLModifier {
         currentOffset += currentTextNodeLength;
         offsetAtEndOfCurrentTextNode = currentOffset + currentTextNodeLength;
       }
-
     }
 
+    logTextRanges(limen.textRangeList);
+//    logTextNodes(limen.textNodeList);
     goOn = true;
     while (goOn) {
       if (offsetAtEndOfCurrentTextNode < endOffset) {
@@ -71,32 +91,81 @@ public class LMNLModifier {
 
       } else {
         int tailLength = offsetAtEndOfCurrentTextNode - endOffset;
-
-//        LOG.info("cursor.getCurrentTextLength()={}", cursor.getCurrentTextLength());
-//        LOG.info("textSize={}", offsetAtEndOfCurrentTextNode);
         int headLength = cursor.getCurrentTextLength() - tailLength;
-        String headText = cursor.getCurrentText().substring(0, headLength);
-        String tailText = cursor.getCurrentText().substring(headLength);
 
         if (tailLength > 0) {
-          // detach head
-          cursor.getCurrentTextNode().setContent(tailText);
-          TextNode newTextNode = new TextNode(headText);
-          TextNode previousTextNode = cursor.getCurrentTextNode().getPreviousTextNode();
-          newTextNode.setNextTextNode(cursor.getCurrentTextNode());
-          newTextNode.setPreviousTextNode(previousTextNode);
-          cursor.getCurrentTextNode().setPreviousTextNode(newTextNode);
-          limen.getTextRanges(cursor.getCurrentTextNode()).forEach(tr -> limen.associateTextWithRange(newTextNode, tr));
-          limen.associateTextWithRange(newTextNode, newTextRange);
-          limen.textNodeList.add(cursor.getTextNodeIndex(), newTextNode);
-          limen.disAssociateTextWithRange(cursor.getCurrentTextNode(),newTextRange);
+          if (headLength > 0) {
+            // detach head
+            String headText = cursor.getCurrentText().substring(0, headLength);
+            String tailText = cursor.getCurrentText().substring(headLength);
+            cursor.getCurrentTextNode().setContent(tailText);
+            TextNode newHeadNode = new TextNode(headText);
+            TextNode previousTextNode = cursor.getCurrentTextNode().getPreviousTextNode();
+            newHeadNode.setNextTextNode(cursor.getCurrentTextNode());
+            newHeadNode.setPreviousTextNode(previousTextNode);
+            if (previousTextNode != null) {
+              previousTextNode.setNextTextNode(newHeadNode);
+            }
+            cursor.getCurrentTextNode().setPreviousTextNode(newHeadNode);
+            limen.getTextRanges(cursor.getCurrentTextNode()).forEach(tr -> {
+              limen.associateTextWithRange(newHeadNode, tr);
+              tr.addTextNode(newHeadNode);
+            });
+            limen.associateTextWithRange(newHeadNode, newTextRange);
+            limen.textNodeList.add(cursor.getTextNodeIndex(), newHeadNode);
+            limen.disAssociateTextWithRange(cursor.getCurrentTextNode(), newTextRange);
+            newTextRange.addTextNode(newHeadNode);
+          } else {
+            limen.associateTextWithRange(cursor.getCurrentTextNode(), newTextRange);
+            newTextRange.addTextNode(cursor.getCurrentTextNode());
+          }
         }
         goOn = false;
-
       }
     }
-
+//    logTextNodes(limen.textNodeList);
+    limen.textRangeList.add(newTextRange);
+    logTextRanges(limen.textRangeList);
   }
+
+  public void addTextRange(TextRange newTextRange, Collection<Position> positions) {
+    if (!newTextRange.hasId()) {
+      throw new RuntimeException("TextRange " + newTextRange.getTag() + " should have an id.");
+    }
+    positions.forEach(position -> {
+      LOG.info("position={}", position);
+      logTextNodes(limen.textNodeList);
+      logTextRanges(limen.textRangeList);
+      addTextRange(newTextRange, position);
+    });
+    logTextNodes(limen.textNodeList);
+    logTextRanges(limen.textRangeList);
+  }
+
+  private void logTextNodes(List<TextNode> list) {
+    StringBuilder textnodes = new StringBuilder();
+    list.forEach(tn -> {
+      if (tn.getPreviousTextNode() != null) {
+        textnodes.append("\"").append(tn.getPreviousTextNode().getContent()).append("\" -> ");
+      }
+      textnodes.append("[").append(tn.getContent()).append("]");
+      if (tn.getNextTextNode() != null) {
+        textnodes.append(" -> \"").append(tn.getNextTextNode().getContent()).append("\"");
+      }
+      textnodes.append("\n");
+    });
+    LOG.info("\nTextNodes:\n{}", textnodes);
+  }
+
+  private void logTextRanges(List<TextRange> list) {
+    StringBuilder textranges = new StringBuilder();
+    list.forEach(tr -> {
+      textranges.append("[").append(tr.getTag()).append("}\n");
+      tr.textNodes.forEach(tn -> textranges.append("  \"").append(tn.getContent()).append("\"\n"));
+    });
+    LOG.info("\nTextRanges:\n{}", textranges);
+  }
+
 
   class TextNodeCursor {
     private TextNode currentTextNode;
--- src/test/java/nl/knaw/huygens/alexandria/lmnl/modifier/LMNLModifierTest.java
+++ src/test/java/nl/knaw/huygens/alexandria/lmnl/modifier/LMNLModifierTest.java
@@ -2,6 +2,7 @@ package nl.knaw.huygens.alexandria.lmnl.modifier;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import nl.knaw.huygens.alexandria.lmnl.data_model.Annotation;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,6 +11,9 @@ import nl.knaw.huygens.alexandria.lmnl.data_model.Document;
 import nl.knaw.huygens.alexandria.lmnl.data_model.TextRange;
 import nl.knaw.huygens.alexandria.lmnl.exporter.LMNLExporter;
 import nl.knaw.huygens.alexandria.lmnl.importer.LMNLImporter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class LMNLModifierTest {
   static final Logger LOG = LoggerFactory.getLogger(LMNLModifierTest.class);
@@ -34,6 +38,20 @@ public class LMNLModifierTest {
     LOG.info("modifiedLMNL={}", modifiedLMNL);
     assertThat(document.value().textNodeList.toString()).isEqualTo("[\"The rain in \", \"Spain\", \" falls mainly on the plain.\"]");
     assertThat(modifiedLMNL).isEqualTo("[text}The rain in [country}Spain{country] falls mainly on the plain.{text]");
+
+    List<Position> positions = new ArrayList<>();
+    positions.add(calculatePosition(content,"plain"));
+//    positions.add(calculatePosition(content,"Spain"));
+    positions.add(calculatePosition(content,"rain"));
+    TextRange rhymeRange = new TextRange(document.value(),"rhyme=r1");
+    rhymeRange.addAnnotation(new Annotation("suffix","ain"));
+
+    modifier.addTextRange(rhymeRange, positions);
+    modifiedLMNL = exporter.toLMNL(document);
+    LOG.info("document.value().textNodeList={}", document.value().textNodeList);
+    LOG.info("modifiedLMNL={}", modifiedLMNL);
+    assertThat(document.value().textNodeList.toString()).isEqualTo("[\"The \", \"rain\", \" in \", \"Spain\", \" falls mainly on the \", \"plain\", \".\"]");
+    assertThat(modifiedLMNL).isEqualTo("[text}The [rhyme=r1 [suffix}ain{suffix]}rain{rhyme=r1] in [country}[rhyme=r1 [suffix}ain{suffix]}Spain{rhyme=r1]{country] falls mainly on the [rhyme=r1 [suffix}ain{suffix]}plain{rhyme=r1].{text]");
   }
 
   private Position calculatePosition(String content, String substring) {
