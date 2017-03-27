f028b8777eeef9d6d7ab845bca36090bb6d79c04
Bram Buitendijk <bram.buitendijk@huygens.knaw.nl>
2017-03-27 10:57:48 +0200

    [NLA-357] Move server packages to alexandria-markup-dropwizard module

--- src/main/java/nl/knaw/huygens/alexandria/dropwizard/resources/RootPaths.java
+++
@@ -1,6 +0,0 @@
-package nl.knaw.huygens.alexandria.dropwizard.resources;
-
-public class RootPaths {
-  public static final String ABOUT = "about";
-  public static final String DOCUMENTS = "documents";
-}
