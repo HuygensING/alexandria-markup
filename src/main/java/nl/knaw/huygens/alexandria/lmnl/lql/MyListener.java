d24ae7fc8b2c99a02168d742dcd253237d3db043
Bram Buitendijk <bram.buitendijk@huygens.knaw.nl>
2017-04-26 17:29:09 +0200

    [NLA-377] cleanup

--- src/main/java/nl/knaw/huygens/alexandria/lmnl/lql/MyListener.java
+++
@@ -1,7 +0,0 @@
-package nl.knaw.huygens.alexandria.lmnl.lql;
-
-import nl.knaw.huygens.alexandria.lmnl.grammar.LQLBaseListener;
-
-public class MyListener extends LQLBaseListener {
-
-}
--- src/main/java/nl/knaw/huygens/alexandria/lmnl/lql/MyVisitor.java
+++
@@ -1,6 +0,0 @@
-package nl.knaw.huygens.alexandria.lmnl.lql;
-
-import nl.knaw.huygens.alexandria.lmnl.grammar.LQLBaseVisitor;
-
-public class MyVisitor extends LQLBaseVisitor {
-}
