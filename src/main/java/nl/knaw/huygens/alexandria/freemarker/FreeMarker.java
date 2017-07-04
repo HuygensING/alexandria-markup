e5acf3cd40449a4a8144a3022afb13adc48abd7c
Bram Buitendijk <bram.buitendijk@huygens.knaw.nl>
2017-07-04 18:04:51 +0200

    [NLA-404] Add licensing plugin settings + headers

--- src/main/java/nl/knaw/huygens/alexandria/freemarker/FreeMarker.java
+++ src/main/java/nl/knaw/huygens/alexandria/freemarker/FreeMarker.java
@@ -1,16 +1,29 @@
 package nl.knaw.huygens.alexandria.freemarker;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.StringWriter;
-import java.io.Writer;
+/*
+ * #%L
+ * alexandria-markup
+ * =======
+ * Copyright (C) 2016 - 2017 Huygens ING (KNAW)
+ * =======
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
 
-import freemarker.template.Configuration;
-import freemarker.template.DefaultObjectWrapper;
-import freemarker.template.Template;
-import freemarker.template.TemplateException;
-import freemarker.template.Version;
+
+import freemarker.template.*;
+
+import java.io.*;
 
 public class FreeMarker {
   private static final Version VERSION = Configuration.VERSION_2_3_25;
