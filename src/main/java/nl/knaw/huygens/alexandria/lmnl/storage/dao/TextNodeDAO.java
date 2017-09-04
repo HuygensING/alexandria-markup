6e020c9d74f9f4996c04af8bf985187085345e16
Bram Buitendijk <bram.buitendijk@huygens.knaw.nl>
2017-09-04 11:27:37 +0200

    DTO -> DAO

---
+++ src/main/java/nl/knaw/huygens/alexandria/lmnl/storage/dao/TextNodeDAO.java
@@ -0,0 +1,44 @@
+package nl.knaw.huygens.alexandria.lmnl.storage.dao;
+
+import com.sleepycat.persist.model.Entity;
+import com.sleepycat.persist.model.PrimaryKey;
+
+@Entity
+public class TextNodeDAO {
+  @PrimaryKey(sequence = "textnode_pk_sequence")
+  private long id;
+
+  private String text;
+
+  private long prevTextNodeId;
+  private long nextTextNodeId;
+
+  public long getId() {
+    return id;
+  }
+
+  public String getText() {
+    return text;
+  }
+
+  public void setText(String text) {
+    this.text = text;
+  }
+
+  public long getPrevTextNodeId() {
+    return prevTextNodeId;
+  }
+
+  public void setPrevTextNodeId(long prevId) {
+    this.prevTextNodeId = prevId;
+  }
+
+  public long getNextTextNodeId() {
+    return nextTextNodeId;
+  }
+
+  public void setNextTextNodeId(long nextId) {
+    this.nextTextNodeId = nextId;
+  }
+
+}
--- src/test/java/nl/knaw/huygens/alexandria/lmnl/storage/DataAccessorTest.java
+++ src/test/java/nl/knaw/huygens/alexandria/lmnl/storage/DataAccessorTest.java
@@ -1,8 +1,8 @@
 package nl.knaw.huygens.alexandria.lmnl.storage;
 
 import com.sleepycat.persist.EntityCursor;
-import nl.knaw.huygens.alexandria.lmnl.storage.dto.LimenDTO;
-import nl.knaw.huygens.alexandria.lmnl.storage.dto.TextNodeDTO;
+import nl.knaw.huygens.alexandria.lmnl.storage.dao.LimenDAO;
+import nl.knaw.huygens.alexandria.lmnl.storage.dao.TextNodeDAO;
 import org.junit.Test;
 
 import java.io.File;
@@ -16,18 +16,19 @@ public class DataAccessorTest {
   public void testDataAccessor() {
     DbEnv dbEnv = new DbEnv();
     File file = new File("out/bdb");
+    file.mkdirs();
     dbEnv.setup(file, false);
     DataAccessor da = new DataAccessor(dbEnv.getEntityStore());
-    LimenDTO limen = new LimenDTO();
-    TextNodeDTO textNode = new TextNodeDTO();
+    LimenDAO limen = new LimenDAO();
+    TextNodeDAO textNode = new TextNodeDAO();
     textNode.setText("Alea acta est");
     try {
       da.textNodeById.put(textNode);
       limen.getTextNodeIds().add(textNode.getId());
       da.limenById.put(limen);
-      LimenDTO limen2 = da.limenById.get(limen.getId());
+      LimenDAO limen2 = da.limenById.get(limen.getId());
       assertThat(limen).isEqualToComparingFieldByField(limen);
-      TextNodeDTO tn2 = da.textNodeById.get(limen2.getTextNodeIds().get(0));
+      TextNodeDAO tn2 = da.textNodeById.get(limen2.getTextNodeIds().get(0));
       assertThat(tn2.getText()).isEqualTo("Alea acta est");
       System.out.println("limen.id=" + limen2.getId());
 
