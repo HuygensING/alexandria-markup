c001e8d16c1871f4b3c68d8636526264ade3ca60
Bram Buitendijk <bram.buitendijk@di.huc.knaw.nl>
2018-12-06 17:41:20 +0100

    remove fail()

--- main/src/test/java/nl/knaw/huygens/alexandria/compare/VariantGraphVisualizerTest.java
+++ main/src/test/java/nl/knaw/huygens/alexandria/compare/VariantGraphVisualizerTest.java
@@ -72,7 +72,6 @@ public class VariantGraphVisualizerTest extends AlexandriaBaseStoreTest {
 
   @Test
   public void test3() {
-    fail();
     String originText = "[FRAGML|+M>\n" +
         "[body|M>\n" +
         "[s|M>Une belle main de femme, élégante et fine, malgré [del|M>l'agrandissement du<del] close-up.\n" +
