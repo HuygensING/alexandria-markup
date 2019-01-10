1ce3ceffbf0b28cab3695c481bb558ee72aedb8e
Bram Buitendijk <bram.buitendijk@di.huc.knaw.nl>
2019-01-10 16:28:13 +0100

    Update copyright headers

--- main/src/test/java/nl/knaw/huc/di/tag/xml/exporter/XMLExporterTest.java
+++ main/src/test/java/nl/knaw/huc/di/tag/xml/exporter/XMLExporterTest.java
@@ -4,7 +4,7 @@ package nl.knaw.huc.di.tag.xml.exporter;
  * #%L
  * alexandria-markup-core
  * =======
- * Copyright (C) 2016 - 2018 HuC DI (KNAW)
+ * Copyright (C) 2016 - 2019 HuC DI (KNAW)
  * =======
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
