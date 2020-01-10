eada54f330d332eb58e16454f2467f2c46718f7b
Bram Buitendijk <bram.buitendijk@di.huc.knaw.nl>
2020-01-10 17:06:49 +0100

    update copyright headers

--- main/src/test/java/nl/knaw/huc/di/tag/tagml/rdf/RDFFactoryTest.java
+++ main/src/test/java/nl/knaw/huc/di/tag/tagml/rdf/RDFFactoryTest.java
@@ -4,7 +4,7 @@ package nl.knaw.huc.di.tag.tagml.rdf;
  * #%L
  * alexandria-markup-core
  * =======
- * Copyright (C) 2016 - 2019 HuC DI (KNAW)
+ * Copyright (C) 2016 - 2020 HuC DI (KNAW)
  * =======
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
