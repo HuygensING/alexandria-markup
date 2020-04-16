166488d8aedb1495ab9cd3dc454cbb5b2cb69462
Bram Buitendijk <bram.buitendijk@huygens.knaw.nl>
2020-04-16 20:12:56 +0200

    convert to kotlin

--- main/src/test/java/nl/knaw/AntlrUtils.kt
+++ main/src/test/java/nl/knaw/AntlrUtils.kt
@@ -1,4 +1,4 @@
-package nl.knaw;
+package nl.knaw
 
 /*-
  * #%L
@@ -19,39 +19,39 @@ package nl.knaw;
  * limitations under the License.
  * #L%
  */
-import de.vandermeer.asciitable.AsciiTable;
-import de.vandermeer.asciitable.CWC_LongestLine;
-import de.vandermeer.asciithemes.a7.A7_Grids;
-import de.vandermeer.skb.interfaces.transformers.textformat.TextAlignment;
-import org.antlr.v4.runtime.Lexer;
-import org.antlr.v4.runtime.Token;
 
-public class AntlrUtils {
+import de.vandermeer.asciitable.AsciiTable
+import de.vandermeer.asciitable.CWC_LongestLine
+import de.vandermeer.asciithemes.a7.A7_Grids
+import de.vandermeer.skb.interfaces.transformers.textformat.TextAlignment
+import org.antlr.v4.runtime.Lexer
+import org.antlr.v4.runtime.Token
 
-  public static String makeTokenTable(Lexer lexer) {
-    AsciiTable table = new AsciiTable()
-        .setTextAlignment(TextAlignment.LEFT);
-    CWC_LongestLine cwc = new CWC_LongestLine();
-    table.getRenderer().setCWC(cwc);
-    table.addRule();
-    table.addRow("Pos", "Text", "Rule", "Next mode", "Token");
-    table.addRule();
+object AntlrUtils {
 
-    Token token;
+  @JvmStatic
+  fun makeTokenTable(lexer: Lexer): String {
+    val table = AsciiTable()
+        .setTextAlignment(TextAlignment.LEFT)
+    val cwc = CWC_LongestLine()
+    table.renderer.cwc = cwc
+    table.addRule()
+    table.addRow("Pos", "Text", "Rule", "Next mode", "Token")
+    table.addRule()
+    var token: Token
     do {
-      token = lexer.nextToken();
-//      LOG.info(token.toString());
-      if (token.getType() != Token.EOF) {
-        String pos = token.getLine() + ":" + token.getCharPositionInLine();
-        String text = "'" + token.getText() + "'";
-        String rule = lexer.getRuleNames()[token.getType() - 1];
-        String mode = lexer.getModeNames()[lexer._mode];
-        table.addRow(pos, text, rule, mode, token);
+      token = lexer.nextToken()
+      //      LOG.info(token.toString());
+      if (token.type != Token.EOF) {
+        val pos = token.line.toString() + ":" + token.charPositionInLine
+        val text = "'" + token.text + "'"
+        val rule = lexer.ruleNames[token.type - 1]
+        val mode = lexer.modeNames[lexer._mode]
+        table.addRow(pos, text, rule, mode, token)
       }
-    } while (token.getType() != Token.EOF);
-    table.addRule();
-    table.getContext().setGrid(A7_Grids.minusBarPlusEquals());
-    return table.render();
+    } while (token.type != Token.EOF)
+    table.addRule()
+    table.context.grid = A7_Grids.minusBarPlusEquals()
+    return table.render()
   }
-
 }
